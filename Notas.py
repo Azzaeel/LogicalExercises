3e486f1b34ac03cc10abb53cfb9b949d61785ded
Azzaeel <jhonatandzm@gmail.com>
2022-03-16 20:41:25 -0500

    2

--- Notas.py
+++ Notas.py
@@ -667,10 +667,10 @@
 
 #36: Tabla de multiplicar
 
-for i in range(1, 11):
-   for j in range(1, 11):
-       print('{:3}'.format(i*j), end=' ')
-   print()
+#for i in range(1, 11):
+#   for j in range(1, 11):
+#       print('{:3}'.format(i*j), end=' ')
+#   print()
 
 
 #38: divisores
